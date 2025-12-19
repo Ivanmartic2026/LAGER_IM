1edf189833954d9dea570cb1e83004da464023b7
IM Vision Group AB <imvisiongroupab@gmail.com>
2025-12-19 17:33:07 +0000

    File changes

--- src/components/labels/ShelfLabel.jsx
+++ src/components/labels/ShelfLabel.jsx
@@ -1,52 +1,45 @@
 import React from 'react';
-import { QRCodeSVG } from 'react-qr-code';
 
 // 30x50mm label component
 export default function ShelfLabel({ article }) {
-  // Generate URL that opens the article directly in Find page
-  const articleUrl = `${window.location.origin}/#/Find?articleId=${article.id}`;
-  
   return (
-    <div className="w-[50mm] h-[30mm] bg-white border border-gray-300 p-1.5 flex items-center gap-2 overflow-hidden">
-      {/* QR Code - left side */}
-      <div className="flex-shrink-0">
-        <QRCodeSVG 
-          value={articleUrl}
-          size={80}
-          level="M"
-        />
-      </div>
+    <div className="w-[50mm] h-[30mm] bg-white border-2 border-black p-2 flex flex-col justify-between">
+      {/* Shelf Location - Most prominent */}
+      {article.shelf_address && (
+        <div className="font-bold text-[20px] text-black text-center border-b-2 border-black pb-1">
+          {article.shelf_address}
+        </div>
+      )}
       
-      {/* Info - right side */}
-      <div className="flex-1 min-w-0 flex flex-col justify-center text-black text-[8px] leading-tight">
-        {/* Shelf Location - Most prominent */}
-        {article.shelf_address && (
-          <div className="font-bold text-[14px] mb-0.5 truncate">
-            {article.shelf_address}
-          </div>
-        )}
-        
+      <div className="flex-1 flex flex-col justify-center text-black">
         {/* Batch Number */}
         {article.batch_number && (
-          <div className="font-semibold text-[9px] truncate">
+          <div className="font-bold text-[12px] truncate text-center">
             #{article.batch_number}
           </div>
         )}
         
         {/* Name */}
         {article.name && (
-          <div className="text-[7px] truncate mt-0.5">
+          <div className="text-[10px] truncate mt-1 text-center font-semibold">
             {article.name}
           </div>
         )}
         
         {/* Manufacturer */}
         {article.manufacturer && (
-          <div className="text-[7px] text-gray-600 truncate">
+          <div className="text-[9px] text-gray-700 truncate mt-0.5 text-center">
             {article.manufacturer}
           </div>
         )}
       </div>
+      
+      {/* Stock quantity if available */}
+      {article.stock_qty !== undefined && (
+        <div className="text-[10px] text-black text-center font-semibold border-t border-gray-400 pt-1">
+          Lager: {article.stock_qty} st
+        </div>
+      )}
     </div>
   );
 }
