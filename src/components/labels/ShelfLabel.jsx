806e95663a9de4771b59d982200a307e6cff067c
IM Vision Group AB <imvisiongroupab@gmail.com>
2025-12-30 16:11:25 +0000

    File changes

--- src/components/labels/ShelfLabel.jsx
+++ src/components/labels/ShelfLabel.jsx
@@ -1,15 +1,16 @@
 import React, { useEffect, useRef } from 'react';
 import QRCode from 'qrcode';
 
-// 30x50mm label component
+// 40x30mm label optimized for shelf printing
 export default function ShelfLabel({ article }) {
   const qrRef = useRef(null);
 
   useEffect(() => {
     if (qrRef.current && article.batch_number) {
       QRCode.toCanvas(qrRef.current, article.batch_number, {
-        width: 60,
-        margin: 0,
+        width: 90,
+        margin: 1,
+        errorCorrectionLevel: 'M',
         color: {
           dark: '#000000',
           light: '#ffffff'
@@ -19,42 +20,37 @@ export default function ShelfLabel({ article }) {
   }, [article.batch_number]);
 
   return (
-    <div className="w-[50mm] h-[30mm] bg-white border-2 border-black p-2 flex text-black">
+    <div className="w-[40mm] h-[30mm] bg-white border-2 border-black p-1.5 flex text-black">
       {/* Left side - Text info */}
-      <div className="flex-1 flex flex-col justify-center pr-2">
+      <div className="flex-1 flex flex-col justify-between pr-1.5">
         {/* Shelf Location - Most prominent */}
         {article.shelf_address && (
-          <div className="font-bold text-[18px] truncate leading-tight mb-1">
+          <div className="font-black text-[20px] truncate leading-none">
             {article.shelf_address}
           </div>
         )}
         
-        {/* Batch Number */}
-        {article.batch_number && (
-          <div className="font-semibold text-[11px] truncate mb-1">
-            #{article.batch_number}
-          </div>
-        )}
-        
-        {/* Name */}
-        {article.name && (
-          <div className="text-[9px] truncate font-medium leading-tight mb-0.5">
-            {article.name}
-          </div>
-        )}
-        
-        {/* Manufacturer */}
-        {article.manufacturer && (
-          <div className="text-[8px] text-gray-600 truncate">
-            {article.manufacturer}
-          </div>
-        )}
+        <div className="space-y-0.5">
+          {/* Batch Number */}
+          {article.batch_number && (
+            <div className="font-bold text-[10px] truncate leading-none">
+              {article.batch_number}
+            </div>
+          )}
+          
+          {/* Name */}
+          {article.name && (
+            <div className="text-[7px] truncate font-medium leading-tight">
+              {article.name.length > 35 ? article.name.substring(0, 35) + '...' : article.name}
+            </div>
+          )}
+        </div>
       </div>
 
       {/* Right side - QR Code */}
       {article.batch_number && (
         <div className="flex items-center justify-center">
-          <canvas ref={qrRef} className="w-[60px] h-[60px]" />
+          <canvas ref={qrRef} className="w-[90px] h-[90px]" />
         </div>
       )}
     </div>
